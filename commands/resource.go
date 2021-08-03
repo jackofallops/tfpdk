7bc08456e91fcac947402fe084d503f1320a5daa
jackofallops <ste@hashicorp.com>
2021-08-03 07:46:01 +0100

    flip resource gen to flags from raw args parsing

--- commands/resource.go
+++ commands/resource.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"strings"
@@ -18,118 +19,97 @@ type ResourceCommand struct {
 
 var _ cli.Command = ResourceCommand{}
 
-type TypedResourceData struct {
-	Name           string
-	ServicePackage string
+type ResourceData struct {
 	HasUpdate      bool
+	Name           string
 	ProviderName   string
+	ServicePackage string
 	Typed          bool
 }
 
+func (d *ResourceData) ParseArgs(args []string) (errors []error) {
+	resourceSet := flag.NewFlagSet("resource", flag.ExitOnError)
+	resourceSet.StringVar(&d.Name, "name", "", "(Required) the name of the new resource, can be in the form resource_name, ResourceName, or resource-name")
+	resourceSet.StringVar(&d.ServicePackage, "servicepackage", "", "(Optional) place the resource under the named service package")
+	resourceSet.BoolVar(&d.HasUpdate, "has-update", true, "(Optional) Use if the new resource supports updating in place. Note if not used all schema properties must be 'ForceNew: true'")
+	resourceSet.BoolVar(&d.Typed, "typed", false, "(Optional) Generate a resource for use with the Typed Resource SDK")
+	err := resourceSet.Parse(args)
+	if err != nil {
+		errors = append(errors, err)
+		return errors
+	}
+
+	if d.Name == "" {
+		errors = append(errors, fmt.Errorf("required option `-name` missing\n"))
+	}
+
+	return errors
+}
+
 func (r ResourceCommand) Run(args []string) int {
-	data := TypedResourceData{}
+	data := &ResourceData{}
 
-	providerName, err := helpers.ProviderName()
-	if err != nil {
-		fmt.Printf("determining provider name: %+v", err)
-		return 1
-	}
-	data.ProviderName = *providerName
-
-	if len(args) == 0 {
-		fmt.Print(r.Help())
-		return 1
-	}
-
-	for _, v := range args {
-		arg := strings.Split(v, "=")
-		if len(arg) > 2 {
-			fmt.Printf("malformed argument %q", arg)
-			return 1
+	if err := data.ParseArgs(args); err != nil {
+		for _, e := range err {
+			r.Ui.Error(e.Error())
 		}
-
-		switch strings.ToLower(strings.TrimLeft(arg[0], "-")) {
-		case "help":
-			fmt.Printf(r.Help())
-			return 0
-		case "name":
-			if len(arg) == 2 {
-				data.Name = arg[1]
-			} else {
-				fmt.Println("argument `name` requires a value, eg `-name=some_resource_name`")
-				return 1
-			}
-
-		case "has-update":
-			data.HasUpdate = true
-
-		case "servicepackage":
-			data.ServicePackage = arg[1]
-
-		case "typed":
-			data.Typed = true
-
-		default:
-			fmt.Printf("unrecognised option %q", arg[0])
-			return 1
-		}
-	}
-
-	if data.Name == "" {
-		fmt.Printf("Error: missing required argument `-name`")
 		return 1
 	}
 
-	tpl := template.Must(template.New("resource.gotpl").Funcs(TplFuncMap).ParseFS(Templatedir, "templates/resource.gotpl"))
-
-	// output file - Note we assume that the PATH to the file already exists, having been through init and, optionally, service package creation
-	outputPath := ""
-	if data.ServicePackage != "" {
-		outputPath = fmt.Sprintf("%s/internal/services/%s/%s_resource.go", strings.ToLower(data.ProviderName), strings.ToLower(strcase.ToCamel(data.ServicePackage)), strcase.ToSnake(data.Name))
-	} else {
-		outputPath = fmt.Sprintf("%s/internal/%s_resource.go", data.ProviderName, strcase.ToSnake(data.Name))
-	}
-
-	if _, err := os.Stat(outputPath); err == nil {
-		fmt.Printf("Error: A resource with this name already exists and will not be overwritten. Please remove this file if you wish to regenerate.")
+	if err := data.generate(); err != nil {
+		r.Ui.Error(fmt.Sprintf("generating resource %s: %+v", data.Name, err))
 		return 1
-	}
-
-	f, err := os.Create(outputPath)
-	if err != nil {
-		fmt.Printf("Error: failed opening output resource file for writing: %+v", err.Error())
-		return 1
-	}
-
-	err = tpl.Execute(f, data)
-	if err != nil {
-		fmt.Println(err.Error())
-		return 1
-	}
-	if err := f.Close(); err != nil {
-		fmt.Printf("Error: Writing to file: %+v", err.Error())
 	}
 
 	return 0
 }
 
+func (d ResourceData) generate() error {
+	providerName, err := helpers.ProviderName()
+	if err != nil {
+		return err
+	}
+	d.ProviderName = *providerName
+
+	tpl := template.Must(template.New("resource.gotpl").Funcs(TplFuncMap).ParseFS(Templatedir, "templates/resource.gotpl"))
+
+	// output file - Note we assume that the PATH to the file already exists, having been through init and, optionally, service package creation
+	outputPath := ""
+	if d.ServicePackage != "" {
+		outputPath = fmt.Sprintf("%s/internal/services/%s/%s_resource.go", strings.ToLower(d.ProviderName), strings.ToLower(strcase.ToCamel(d.ServicePackage)), strcase.ToSnake(d.Name))
+	} else {
+		outputPath = fmt.Sprintf("%s/internal/%s_resource.go", d.ProviderName, strcase.ToSnake(d.Name))
+	}
+
+	if _, err := os.Stat(outputPath); err == nil {
+		return fmt.Errorf("a resource with this name already exists and will not be overwritten. Please remove this file if you wish to regenerate")
+	}
+
+	f, err := os.Create(outputPath)
+	if err != nil {
+		return fmt.Errorf("failed opening output resource file for writing: %+v", err.Error())
+	}
+
+	err = tpl.Execute(f, d)
+	if err != nil {
+		return err
+	}
+	if err := f.Close(); err != nil {
+		fmt.Printf("failed riting to file: %+v", err.Error())
+	}
+
+	return nil
+}
+
 func (r ResourceCommand) Help() string {
-	return fmt.Sprintf(`
+	return `
 Usage: tfpdk resource [options]
 
 Generates a scaffolded resource, optionally under a service package for this provider
 
-Options:
-
--name=string			(Required) the name of the new resource, can be in the form resource_name, ResourceName, or resource-name
-
--servicepackage=string		(Optional) place the resource under the named service package
-
--has-update			(Optional) Use if the new resouce supports updating in place. Note if not used all schema properties must be 'ForceNew: true'
-
--typed				(Optional) Generate a resource for use with the Typed Resource SDK
-
-`)
+Example: 
+$ tfpdk resource -name=MyNewResource -servicepackage=SomeExistingService -typed -has-update
+`
 }
 
 func (r ResourceCommand) Synopsis() string {
