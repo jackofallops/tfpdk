1971b66ba7a5e810cb40affe297200372d4060ad
jackofallops <ste@hashicorp.com>
2021-12-06 07:11:49 +0000

    simplify template

--- helpers/formatters.go
+++ helpers/formatters.go
@@ -54,25 +54,32 @@ func PrefixedLabelString(input string) string {
 	return fmt.Sprintf("%s `%s`", prefix, input)
 }
 
-func SchemaItemFormatter(input Schema, name string) string {
+func SchemaItemFormatter(input interface{}, name string) string {
+	data := Schema{}
+	switch v := input.(type) {
+	case Schema:
+		data = v
+	case map[string]interface{}:
+		data = flattenMapToSchema(v)
+	}
 	// Block detection
 	var optionalOrRequired, desc, forceNew string
-	if input.Required {
+	if data.Required {
 		optionalOrRequired = "(Required) "
-	} else if input.Optional {
+	} else if data.Optional {
 		optionalOrRequired = "(Optional) "
 	}
-	if strings.EqualFold(input.Type, "typelist") || strings.EqualFold(input.Type, "typeset") {
+	if strings.EqualFold(data.Type, "typelist") || strings.EqualFold(data.Type, "typeset") {
 		return fmt.Sprintf("* `%s` %s- %s block as detailed below.", name, optionalOrRequired, PrefixedLabelString(name))
 	}
 
-	if input.Description != "" {
-		desc = strings.TrimSpace(input.Description) // TODO auto-fix line ends for MD linting?
+	if data.Description != "" {
+		desc = strings.TrimSpace(data.Description) // TODO auto-fix line ends for MD linting?
 	} else {
 		desc = "// TODO - Add missing `Description` to schema for this property and regenerate this file."
 	}
 
-	if input.ForceNew {
+	if data.ForceNew {
 		forceNew = "Changing this forces a new resource to be created."
 		return fmt.Sprintf("* `%s` %s- %s %s", name, optionalOrRequired, desc, forceNew)
 	}
@@ -102,4 +109,53 @@ func SchemaItemFormatterSpecial(input Schema, name string) string {
 		return fmt.Sprintf("* `%s` %s - %s", name, optionalOrRequired, desc)
 	}
 	return ""
+}
+
+func flattenMapToSchema(input map[string]interface{}) Schema {
+	output := Schema{}
+	if t, ok := input["type"]; ok {
+		output.Type = t.(string)
+	}
+
+	if t, ok := input["config_mode"]; ok {
+		output.ConfigMode = t.(string)
+	}
+
+	if t, ok := input["optional"]; ok {
+		output.Optional = t.(bool)
+	}
+
+	if t, ok := input["required"]; ok {
+		output.Required = t.(bool)
+	}
+
+	if t, ok := input["default"]; ok {
+		output.Default = t
+	}
+
+	if t, ok := input["description"]; ok {
+		output.Description = t.(string)
+	}
+
+	if t, ok := input["computed"]; ok {
+		output.Computed = t.(bool)
+	}
+
+	if t, ok := input["force_new"]; ok {
+		output.ForceNew = t.(bool)
+	}
+
+	if t, ok := input["elem"]; ok {
+		output.Elem = t
+	}
+
+	if t, ok := input["max_items"]; ok {
+		output.MaxItems = int(t.(float64))
+	}
+
+	if t, ok := input["min_items"]; ok {
+		output.MinItems = int(t.(float64))
+	}
+
+	return output
 }
